d42a3c8562e5415b2b86d6968027b262c9baf625
lubiao147258 <1368321570@qq.com>
2018-03-02 17:36:38 +0800

    添加web的controller层，以及主要的方法

---
+++ src/main/java/com/seckill/dto/SeckillResult.java
@@ -0,0 +1,7 @@
+package com.seckill.dto;
+
+
+//封装json结果
+public class SeckillResult<T> {
+
+}
---
+++ src/main/java/com/seckill/web/SeckillController.java
@@ -0,0 +1,50 @@
+package com.seckill.web;
+
+import com.seckill.entity.Seckill;
+import com.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/seckill") //url:/模块/资源/...
+public class SeckillController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(name = "/list",method = RequestMethod.GET)
+    public String list(Model model){//list.jsp + model = ModelAndView
+        //获取列表页
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list",list);
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail",method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId , Model model){
+        if(seckillId == null){
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if(seckill == null){
+            return "forward:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+    //ajax json
+    @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST)
+    public void/*TODO*/ exposer(Long seckillId){
+
+    }
+}
