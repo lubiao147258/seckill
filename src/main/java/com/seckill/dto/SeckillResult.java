4818bf7a2eef530038894a6c078fc7b35dab0919
lubiao147258 <1368321570@qq.com>
2018-03-05 09:43:40 +0800

    SeckillController层添加返回json格式的数据的方法

--- src/main/java/com/seckill/dto/SeckillResult.java
+++ src/main/java/com/seckill/dto/SeckillResult.java
@@ -1,7 +1,46 @@
 package com.seckill.dto;
 
 
-//封装json结果
+//所有的ajax请求的返回类型，封装json结果的VO
 public class SeckillResult<T> {
 
+    private boolean success;
+
+    private T data;
+
+    private String msg;
+
+    public SeckillResult(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    public SeckillResult(boolean success, String msg) {
+        this.success = success;
+        this.msg = msg;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
--- src/main/java/com/seckill/web/SeckillController.java
+++ src/main/java/com/seckill/web/SeckillController.java
@@ -1,15 +1,18 @@
 package com.seckill.web;
 
+import com.seckill.dto.Exposer;
+import com.seckill.dto.SeckillExecution;
+import com.seckill.dto.SeckillResult;
 import com.seckill.entity.Seckill;
+import com.seckill.execption.RepeatKillException;
+import com.seckill.execption.SeckillCloseException;
 import com.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,8 +46,46 @@ public class SeckillController {
     }
 
     //ajax json
-    @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST)
-    public void/*TODO*/ exposer(Long seckillId){
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId){
+        SeckillResult<Exposer> result;
+        try {
+            Exposer exposer = this.seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true, exposer);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            result = new SeckillResult<Exposer>(false, e.getMessage());
+        }
+        return result;
+    }
 
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId,
+                                                   @PathVariable("md5") String md5,
+                                                   @CookieValue(value = "killPhone",required = false) Long phone){
+        if (phone == null){
+            return new SeckillResult<SeckillExecution>(false, "未注册");
+        }
+        SeckillResult<SeckillExecution> result;
+        try {
+            SeckillExecution seckillExecution = this.seckillService.executeSeckill(seckillId, phone, md5);
+            return new SeckillResult<SeckillExecution>(true, seckillExecution);
+        } catch(RepeatKillException e){
+
+        } catch(SeckillCloseException e){
+
+        } catch (Exception e){
+            logger.error(e.getMessage());
+            //TODO
+            //result = new SeckillResult<SeckillExecution>(false, e.getMessage());
+        }
+        return null;
     }
 }
